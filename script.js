9ae887ad30d894b2cd7b3e9c3c940569257bbbde
Malik Sadiki-Torres <maliktorres98@gmail.com>
2023-08-27 18:13:58 -0400

    added again click functionality

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value); 
 */
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,7 +23,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -52,3 +52,24 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+
+document.querySelector('.again').addEventListener('click', function() {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = '20';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+})
+
+// First Solution
+// document.querySelector('.again').addEventListener('click', function () {
+//   document.querySelector('.score').textContent = '20';
+//   document.querySelector('.number').style.width = '15rem';
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('.guess').value = '';
+//   document.querySelector('.message').textContent = 'Start guessing...';
+//   document.querySelector('body').style.backgroundColor = '#222';
+// });
